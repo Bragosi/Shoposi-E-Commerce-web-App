02f74d43ba4a83a2353917d83c2c02b7afeb03e1
Bragosi <oladejoboluwatife20003@gmail.com>
2025-07-29 17:14:47 +0100

    working on categories

---
+++ Backend/controller/products/FilterProduct.js
@@ -0,0 +1,11 @@
+const filterProductController = async(req, res)=>{
+    try {
+        
+    } catch (error) {
+            res.status(400).json({
+              message: error.message || error,
+              error: true,
+              success: false,
+            });
+          }
+}
--- my-project/src/pages/ProductCategoryPage.jsx
+++ my-project/src/pages/ProductCategoryPage.jsx
@@ -1,69 +1,69 @@
-import { useParams } from "react-router-dom";
-import productCategory from "../Helpers/ProductCategory";
-import { useState } from "react";
-import RecommendedProduct from "../components/RecommendedProduct";
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import productCategory from '../Helpers/ProductCategory'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 const ProductCategoryPage = () => {
-  const params = useParams();
-  console.log('params', params)
-  const [data, setdata] = useState([]);
-  const [loading, setloading] = useState(false);
+    const params = useParams()
+    console.log(params?.categoryName)
+    const [data, setdata] = useState([])
+    const [loading, setloading] = useState(false)
 
-  const fetchData = async () => {
-    const response = await fetch();
+    const fetchData = async()=> {
+        const response = await fetch()
 
-    const dataResponse = await response.json();
-    setdata(dataResponse?.data || []);
-    console.log("dataResponse", dataResponse);
-  };
+        const dataResponse = await response.json()
+        console.log('dataRes', dataResponse)
+    }
+      
   return (
-    <div className="container mx-auto p-4">
-      {/** Desktop version */}
-      <div className="hidden lg:grid grid-cols-[200px,1fr]">
-        {/** Left side  */}
-        <div className="bg-white p-2 min-h-[calc(100vh-120px)] overflow-y-scroll ">
-          <div className="text-lg">
-            <h1 className="text-base font-medium uppercase text-slate-500 border-b border-slate-300">
-              Category
-            </h1>
-            <form action="" className="font-sm flex flex-col gap-2 py-2">
-              <div className="flex items-center gap-3">
-                <input type="radio" name="sortBy" />
-                <label htmlFor="">Price- Low to High</label>
-              </div>
+    <div className='container mx-auto p-4'>
+        {/** desktop version */}
+            <div className='hidden lg:grid grid-cols-[200px,1fr]'>
+                {/** Left side  */}
+                <div className='bg-white p-2 min-h-[calc(100vh-120px)] overflow-y-scroll '>
+                    {/** sort By */}
+                    <div className=" ">
+                        <h3 className='text-base uppercase font-medium text-slate-300 border-b pb-1 '>Sort By</h3>
 
-              <div className="flex items-center gap-3">
-                <input type="radio" name="sortBy" />
-                <label htmlFor="">Price- High to Low</label>
-              </div>
-              {productCategory.map((categoryName, index) => {
-                return (
-                  <div className="flex items-center gap-3 ">
-                    <input
-                      type="checkbox"
-                      name={"category"}
-                      id={categoryName.value}
-                    />
-                    <label htmlFor={categoryName.value}>
-                      {categoryName?.label}
-                    </label>
-                  </div>
-                );
-              })}
-            </form>
-          </div>
-        </div>
-        {}
-        <div>
-          {
-            params?.categoryName && (
-          <RecommendedProduct category={params?.categoryName} heading={"Recommended Products"}/>
-            )
-          }
-        </div>
-      </div>
+                        <form action="" className='text-sm flex flex-col gap-2 py-2'>
+                            <div className='flex items-center gap-3 '>
+                                <input type="radio" name='sort' />
+                                <label htmlFor="">Price - Low to High</label>
+                            </div>
+                            <div className='flex items-center gap-3 '>
+                                <input type="radio" name='sort' />
+                                 <label htmlFor="">High - Low Low</label>
+                            </div>
+                        </form>
+                    </div>
+
+                        <div className=" ">
+                        <h3 className='text-base uppercase font-medium text-slate-300 border-b pb-1 '>Category</h3>
+
+                        <form action="" className='text-sm flex flex-col gap-2 py-2'>
+                            {
+                                productCategory.map((categoryName, index)=>{
+                                    return(
+                                        <div className='flex items-center gap-3'>
+                                            <input type="checkbox" id={categoryName.value} name={'category'} />
+                                            <label htmlFor={categoryName.value}> {categoryName?.label} </label>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </form>
+                    </div>
+
+                </div>
+                {/** Right side  */}
+                <div>
+                    right
+                </div>
+            </div>
     </div>
-  );
-};
+  )
+}
 
-export default ProductCategoryPage;
+export default ProductCategoryPage
