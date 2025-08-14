8a37eb2a8f95985063351b6d9ee44696916c7e90
Bragosi <oladejoboluwatife20003@gmail.com>
2025-08-14 21:44:53 +0100

    editing routes

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express.Router();
 
 const userSignUp = require("../controller/user/UserSignUp");
-const UserSignIn = require("../controller/user/UserSignIn");
+const UserSignIn = require("../controller/user/userSignIn");
 const userDetails = require('../controller/user/UserDetails');
 const authToken = require('../middleWare/authToken');
 const userLogOut = require('../controller/user/UserLogOut');
