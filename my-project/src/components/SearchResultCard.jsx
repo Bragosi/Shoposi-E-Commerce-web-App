dfc7a12617140b401cd8f63aa41ee1ee71b4f880
YourGitHubUsername <YourGitHubEmail>
2025-08-16 08:33:45 +0100

    width

--- my-project/src/components/SearchResultCard.jsx
+++ my-project/src/components/SearchResultCard.jsx
@@ -8,7 +8,7 @@ import { useContext } from "react";
 const SearchResultCard = ({ data }) => {
       const { fetchCountCartProduct } = useContext(Context);
   return (
-       <div className="container mx-auto px-4 my-8">
+       <div className="w-full mx-auto px-4 my-8">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
               {data.map((product) => (
                 <Link
